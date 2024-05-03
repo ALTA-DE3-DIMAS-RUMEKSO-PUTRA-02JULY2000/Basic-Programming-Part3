deb57fb15b31e62cc6f69c97c5ce0b8d3dd9e816
masgal <dimasrumekso28@gmail.com>
2024-05-03 23:03:51 +0700

    solution1

--- problem1/main.py
+++ problem1/main.py
@@ -2,6 +2,22 @@
 student_score = 80
 
 # Process: Your Solution Code Here
+inputScore = input("Enter your score : ")
+studentScore = int(inputScore)
 
+if studentScore >= 80 :
+    print("Nilai A")
+
+elif studentScore <= 79 and studentScore >= 65 :
+    print("Nilai B+")
+
+elif studentScore <= 64 and studentScore >= 50 :
+    print("Nilai B")
+
+elif studentScore <=49 and studentScore >= 35 :
+    print("Nilai C")
+
+else :
+    print("Nilai D")
 
 # Output "Nilai A"
