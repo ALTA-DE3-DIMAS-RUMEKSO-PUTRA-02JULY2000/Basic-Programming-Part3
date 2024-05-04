1d35b2689109970cb19a69207f4ce5cbc386649f
masgal <dimasrumekso28@gmail.com>
2024-05-04 14:00:25 +0700

    Solution2.1

--- problem2.1/main.py
+++ problem2.1/main.py
@@ -1,1 +1,6 @@
-bilangan = int(input())
+inputBilangan = input("Faktor bilangan dari : ")
+bilangan = int(inputBilangan)
+
+for i in range(1, bilangan + 1) :
+    if bilangan % i == 0 :
+        print(i)
